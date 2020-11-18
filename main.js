513a205e6c191e74bc3f97f5144823a98f3b0242
songhun115 <hoho77113@daum.net>
2020-11-18 18:56:39 +0900

    헤더부분 작성

---
+++ main.js
@@ -0,0 +1,29 @@
+// nav_item hover하면 menu_bg가 block
+$(document).ready(function(){
+  $('.nav_item').hover(function(){
+    $('.menu_bg').addClass('on');
+    $('.nav_bar').addClass('bottom_line');
+  }  , function(){
+    $('.menu_bg').removeClass('on');
+    $('.nav_bar').removeClass('bottom_line');
+  });
+
+  $('.menu_icon_box').click(function(){
+    
+  const $clickd = $(this);
+  if( $clickd.hasClass('rotate')) {
+    $clickd.removeClass('rotate');
+    $('.open_menu').removeClass('open');
+    $('.nav_item').removeClass('show');
+    $('.icon').removeClass('show');
+  } else {
+    $clickd.addClass('rotate');
+    $('.open_menu').addClass('open');
+    $('.nav_item').addClass('show');
+    $('.icon').addClass('show');
+  }
+  })
+
+
+ 
+});
